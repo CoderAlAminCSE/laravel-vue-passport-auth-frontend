550f866026b3243993c0a3a612661aea0f5c7fb5
Md. Al Amin <98679137+CoderAlAminCSE@users.noreply.github.com>
2023-09-29 12:03:48 +0600

    navbar, login page added

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="bg-light py-4">
+    <div class="container">
+      <p class="m-0">This is footer</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+      <router-link class="navbar-brand" :to="{name:'home'}">
+        <img src="../assets/logo.png" class="w-25" alt="logo">
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name:'home'}">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name:'about'}">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name:'blog'}">Blog</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name:'login'}">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name:'register'}">Register</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name:'logout'}">Logout</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
