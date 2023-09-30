a3de28051cd5edfe257ffd96ea2e1ad29044c179
Md. Al Amin <98679137+CoderAlAminCSE@users.noreply.github.com>
2023-09-30 10:57:42 +0600

    prevent access route by hearcoding in the url without login and access login route when already logged in

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import Logout from '../components/LogoutComponent.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
 
@@ -10,22 +11,26 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: { requiresAuth: true }
   },
   {
     path: '/about',
     name: 'about',
-    component: () => import('../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/login',
     name: 'login',
-    component: LoginView
+    component: LoginView,
+    meta: { visitor: true }
   },
   {
     path: '/logout',
     name: 'logout',
-    component: Logout
+    component: Logout,
+    meta: { requiresAuth: true }
   },
 ]
 
@@ -33,6 +38,29 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.getters.loggedIn) {
+      next({
+        name: 'login'
+      })
+    } else {
+      next()
+    }
+  } else if (to.matched.some(record => record.meta.visitor)) {
+    if (store.getters.loggedIn) {
+      next({
+        name: 'home'
+      })
+    } else {
+      next()
+    }
+  }
+  else {
+    next()
+  }
 })
 
 export default router
